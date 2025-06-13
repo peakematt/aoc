3755d7958ab846dfe3b19d3ef81e860f8db0d013
Matt Peake <peakematt@outlook.com>
2025-06-13 19:30:36 -0400

    simplify

--- 2015/day3/rust/src/main.rs
+++ 2015/day3/rust/src/main.rs
@@ -1,33 +1,35 @@
-use std::collections::HashMap;
+use std::collections::HashSet;
 
 #[derive(Copy, Clone)]
 struct Location {
     x: i32,
     y: i32,
-    visit_count: i32,
-}
-
-impl Location {
-    fn new(x: i32, y: i32) -> Location {
-        Location {
-            x,
-            y,
-            visit_count: 0,
-        }
-    }
-
-    fn visit(&mut self) {
-        self.visit_count += 1
-    }
-
-    fn hash_key(&self) -> String {
-        format!("{},{}", self.x, self.y).to_string()
-    }
 }
 
 struct Visitor {
     current_location: Location,
-    visited_locations: HashMap<String, Location>,
+    visited_locations: HashSet<Location>,
+}
+
+impl Location {
+    fn new(x: i32, y: i32) -> Location {
+        Location { x, y }
+    }
+}
+
+impl PartialEq for Location {
+    fn eq(&self, other: &Self) -> bool {
+        self.x == other.x && self.y == other.y
+    }
+}
+
+impl Eq for Location {}
+
+impl std::hash::Hash for Location {
+    fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
+        self.x.hash(state);
+        self.y.hash(state);
+    }
 }
 
 impl Visitor {
@@ -35,25 +37,22 @@ impl Visitor {
         let first_location = Location::new(0, 0);
         let mut b = Visitor {
             current_location: first_location,
-            visited_locations: HashMap::new(),
+            visited_locations: HashSet::new(),
         };
-        b.visited_locations
-            .insert(first_location.hash_key(), first_location);
+        b.visited_locations.insert(first_location);
         b
     }
 
     fn visit(&mut self, loc: Location) {
-        match self.visited_locations.get_mut(&loc.hash_key()) {
-            Some(location) => location.visit(),
-            None => drop(
-                self.visited_locations
-                    .insert(loc.hash_key(), Location::new(loc.x, loc.y)),
-            ),
+        if self.visited_locations.remove(&loc) {
+            let _ = self.visited_locations.insert(loc);
+        } else {
+            self.visited_locations.insert(Location::new(loc.x, loc.y));
         }
         self.current_location = loc;
     }
 
-    fn handle_move(&mut self, direction: std::string::String) {
+    fn handle_move(&mut self, direction: std::string::String) -> Location {
         match direction.as_ref() {
             "^" => self.visit(Location::new(
                 self.current_location.x,
@@ -74,6 +73,7 @@ impl Visitor {
             "\n" => {}
             other => println!("Unknown input character: '{}'", other),
         }
+        self.current_location
     }
 }
 
@@ -81,41 +81,18 @@ fn main() {
     let mut santa = Visitor::new();
     let mut robo_santa = Visitor::new();
     let mut santa_turn = true;
-    let mut all_locations: HashMap<String, Location> = HashMap::new();
+    let mut all_locations: HashSet<Location> = HashSet::new();
     let input = std::fs::read_to_string("src/input.txt").expect("Should be able to read input");
 
     for c in input.chars() {
         if santa_turn {
-            santa.handle_move(c.to_string());
+            all_locations.insert(santa.handle_move(c.to_string()));
             santa_turn = false;
         } else {
-            robo_santa.handle_move(c.to_string());
+            all_locations.insert(robo_santa.handle_move(c.to_string()));
             santa_turn = true;
         }
     }
 
-    for k in santa.visited_locations.keys() {
-        if all_locations.contains_key(k) {
-            // pass
-        } else {
-            let v = santa
-                .visited_locations
-                .get(k)
-                .expect("Hashmap contains key but no value at that key");
-            all_locations.insert(k.to_owned(), Location::new(v.x, v.y));
-        }
-    }
-    for k in robo_santa.visited_locations.keys() {
-        if all_locations.contains_key(k) {
-            // pass
-        } else {
-            let v = match robo_santa.visited_locations.get(k) {
-                Some(location) => location,
-                None => panic!("failed to get santa's visited location"),
-            };
-            all_locations.insert(k.to_owned(), Location::new(v.x, v.y));
-        }
-    }
-
-    println!("Visited {} locations", all_locations.len())
+    println!("Visited {} locations", all_locations.len());
 }
